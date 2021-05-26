a3519e1001234711b617d20dcbfff14ba0edb7d9
William Moore <w.moore@dundee.ac.uk>
2021-05-26 09:20:57 +0100

    csv_to_roi_table.py assumes ROI ordered
    Don't need to use coordinates to look up each ROI

--- scripts/csv_to_roi_table.py
+++ scripts/csv_to_roi_table.py
@@ -4,7 +4,6 @@ import pandas
 import argparse
 import csv
 import mimetypes
-from shapely.geometry import Polygon, Point
 
 import omero.clients
 import omero.cli
@@ -48,47 +47,6 @@ def populate_metadata(image, csv_name):
     ctx.parse()
 
 
-def get_shapes_at_point(conn, image_id, x, y, z):
-
-    # Get all shapes that contain the point
-    params = omero.sys.ParametersI()
-    params.addId(image_id)
-    params.add('theZ', rint(z))
-    # params.add('x', rdouble(x_pixels))
-    # params.add('y', rdouble(y_pixels))
-    query = """select shape from Shape as shape
-            join shape.roi as roi
-            where roi.image.id = :id
-            and shape.theZ = :theZ"""
-    # for mask
-    #         and shape.x < :x
-    #         and shape.y < :y
-    #         and (shape.width + shape.x) > :x"""
-
-    shapes = conn.getQueryService().findAllByQuery(query, params, conn.SERVICE_OPTS)
-
-    shapes_at_point = []
-    for shape in shapes:
-        if isinstance(shape, omero.model.PolygonI):
-            points = shape.points.val
-            xy = []
-            for p in points.split(" "):
-                xy.append([float(c) for c in p.strip(",").split(",")])
-            if len(xy) > 3 and Polygon(xy).contains(Point(x, y)):
-                shapes_at_point.append(shape)
-
-    return shapes_at_point
-
-
-def get_shape_ids(conn, roi_id):
-    params = omero.sys.ParametersI()
-    params.addId(roi_id)
-    query = """select shape from Shape as shape
-            where shape.roi.id = :id"""
-    shapes = conn.getQueryService().findAllByQuery(query, params, conn.SERVICE_OPTS)
-    return [s.id.val for s in shapes]
-
-
 def set_name_for_all_rois(conn, image_id):
     params = omero.sys.ParametersI()
     params.addId(image_id)
@@ -96,7 +54,8 @@ def set_name_for_all_rois(conn, image_id):
             where roi.image.id = :id"""
     rois = conn.getQueryService().findAllByQuery(query, params, conn.SERVICE_OPTS)
     for roi in rois:
-        roi.name = rstring(str(roi.id.val))
+        if roi.name is None:
+            roi.name = rstring(str(roi.id.val))
     conn.getUpdateService().saveArray(rois)
 
 
@@ -106,15 +65,6 @@ def process_image(conn, image):
 
     # Read csv for each image
     image_name = image.name
-    pix_x = image.getPixelSizeX()   # 0.09
-    pix_y = image.getPixelSizeY()   # 0.09
-    pix_z = image.getPixelSizeZ()   # 0.19
-    print(pix_x, pix_y, pix_z)
-    # hard-coded values that seem to work better
-    pix_x = 0.1
-    pix_y = 0.1
-    pix_z = 0.23
-    print(pix_x, pix_y, pix_z)
 
     table_pth = tables_path % (image_name, image_name)
     print('table_pth', table_pth)
@@ -126,35 +76,29 @@ def process_image(conn, image):
     # Create output table with extra columns
     df2 = pandas.DataFrame(columns=(["Roi", "Shape"] + new_col_names))
 
+    # Get all ROIs - The order seems to correspond to the order they were
+    # created in, which matches the order of rows in the .tsv
+    result = conn.getRoiService().findByImage(image.id, None)
+
     # Go through every row, find Shape ID and ROI ID
     # for row in df.to_dict(orient='records'):
     for index, row in df.iterrows():
-        x_raw = row["Centroids RAW X"]
-        y_raw = row["Centroids RAW Y"]
-        z_raw = row["Centroids RAW Z"]
-        x_pixels = x_raw / pix_x
-        y_pixels = y_raw / pix_y
-        z_index = int(round(z_raw / pix_z))
-
-        polygons = get_shapes_at_point(conn, image.id, x_pixels, y_pixels, z_index)
-        if len(polygons) == 1:
-            poly = polygons[0]
-            print("Found 1 polygon. ID:", poly.id.val)
-            roi = poly.roi
-            new_row = {}
-            # Create a copy of row, with columns named with_underscores (no spaces)
-            for c in col_names:
-                value = row[c]
-                new_row[c.replace(" ", "_")] = value
-            new_row["Roi"] = roi.id.val
-            for shape_id in get_shape_ids(conn, roi.id.val):
-                new_row["Shape"] = shape_id
-                df2 = df2.append(new_row, ignore_index=True)
-            # all ROIs must have a name for populate metadata
-            roi.name = rstring(row["Cell ID"])
-            conn.getUpdateService().saveObject(roi)
-        else:
-            print(" - found polygons:", len(polygons))
+        roi = result.rois[index]
+        print("ROW", index, roi.id.val)
+        new_row = {}
+        # Create a copy of row, with columns named with_underscores (no spaces)
+        for c in col_names:
+            value = row[c]
+            new_row[c.replace(" ", "_")] = value
+        new_row["Roi"] = roi.id.val
+        # for OMERO.figure workshop, we want to be able to query table by Shape ID
+        # So we duplicate the ROI row for each Shape
+        for shape in roi.copyShapes():
+            new_row["Shape"] = shape.id.val
+            df2 = df2.append(new_row, ignore_index=True)
+        # all ROIs must have a name for populate metadata
+        roi.name = rstring(row["Cell ID"])
+        conn.getUpdateService().saveObject(roi)
 
     csv_name = image.name + ".csv"
     print("writing", csv_name)
@@ -187,6 +131,9 @@ def main(conn, args):
 
 
 if __name__ == "__main__":
+    # NB: Assumes that the segmentation ROIs are added to the image
+    # (and *before* any other ROIs)
+    #
     # USAGE:
     # $ cd idr0079-hartmann-lateralline
     # $ python csv_to_roi_table --name 00E41C184C
