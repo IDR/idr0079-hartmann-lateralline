cc6682cb049b89b5df07a807109b85cc7a656cdf
William Moore <w.moore@dundee.ac.uk>
2021-05-26 14:19:31 +0100

    Add scripts/idr0079_voxel_sizes.sh and .tsv

---
+++ scripts/idr0079_voxel_sizes.sh
@@ -0,0 +1,32 @@
+# membrane_only
+omero metadata pixelsize --x 0.0992103 --y 0.0992103 --z 0.2245383 Dataset:10260
+
+# membranes_nuclei
+omero metadata pixelsize --x 0.1024427 --y 0.1024427 --z 0.2245383 Dataset:10262
+
+# membranes_actin
+omero metadata pixelsize --x 0.1024427 --y 0.1024427 --z 0.2245383 Dataset:10264
+
+# membranes_cisgolgi
+omero metadata pixelsize --x 0.0992103 --y 0.0992103 --z 0.22 Dataset:10266
+
+# membranes_earlyendo
+omero metadata pixelsize --x 0.0992103 --y 0.0992103 --z 0.22 Dataset:10268
+
+# membranes_recendo
+omero metadata pixelsize --x 0.0992103 --y 0.0992103 --z 0.2245383 Dataset:10270
+
+# membranes_tgn
+omero metadata pixelsize --x 0.0992103 --y 0.0992103 --z 0.22 Dataset:10272
+
+# membranes_transgolgi
+omero metadata pixelsize --x 0.0992103 --y 0.0992103 --z 0.22 Dataset:10274
+
+# membranes_atoh1a
+omero metadata pixelsize --x 0.0992103 --y 0.0992103 --z 0.2245383 Dataset:10276
+
+# membranes_lyso
+omero metadata pixelsize --x 0.0992103 --y 0.0992103 --z 0.2245383 Dataset:10278
+
+# membranes_pea3fish
+omero metadata pixelsize --x 0.0851964 --y 0.0851964 --z 0.1867357 Dataset:10280
