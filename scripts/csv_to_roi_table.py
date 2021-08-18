4bd7dc66f11ed806093732b5ded532f31243259d
William Moore <w.moore@dundee.ac.uk>
2021-08-18 17:11:14 +0100

    Add table_name parameter to csv_to_roi_table.py

--- scripts/csv_to_roi_table.py
+++ scripts/csv_to_roi_table.py
@@ -15,13 +15,11 @@ import os
 
 project_name = "idr0079-hartmann-lateralline/experimentA"
 
-tables_path = os.path.join(
+base_tables_path = os.path.join(
     os.getcwd(),
     "experimentA/idr0079_experimentA_extracted_measurements/%s/",
-    "%s_other_measurements.tsv"
+    "%s"
 )
-print("tables_path", tables_path)
-
 
 def get_omero_col_type(dtype):
     """Returns s for string, d for double, l for long/int"""
@@ -41,26 +39,11 @@ def populate_metadata(image, csv_name):
     ctx.parse()
 
 
-def set_name_for_all_rois(conn, image_id):
-    params = omero.sys.ParametersI()
-    params.addId(image_id)
-    query = """select roi from Roi as roi
-            where roi.image.id = :id"""
-    rois = conn.getQueryService().findAllByQuery(query, params, conn.SERVICE_OPTS)
-    for roi in rois:
-        if roi.name is None:
-            roi.name = rstring(str(roi.id.val))
-    conn.getUpdateService().saveArray(rois)
-
-
-def process_image(conn, image):
-    # Since populate_metadata fails if rois don't have name
-    set_name_for_all_rois(conn, image.id)
-
+def process_image(conn, image, table_name):
     # Read csv for each image
     image_name = image.name
-
-    table_pth = tables_path % (image_name, image_name)
+    table_pth = base_tables_path % (image_name, image_name)
+    table_pth += table_name
     print('table_pth', table_pth)
     df = pandas.read_csv(table_pth, delimiter="\t")
 
@@ -73,12 +56,17 @@ def process_image(conn, image):
     # Get all ROIs - The order seems to correspond to the order they were
     # created in, which matches the order of rows in the .tsv
     result = conn.getRoiService().findByImage(image.id, None)
+    roi_count = len(result.rois)
+    row_count = len(df.index)
+    print("Found %s ROIS and %s csv rows" % (roi_count, row_count))
+    if roi_count != row_count:
+        return f"Mismatching counts: {table_pth}"
 
     # Go through every row, find Shape ID and ROI ID
     # for row in df.to_dict(orient='records'):
     for index, row in df.iterrows():
+        # print("ROW", index, "/", len(result.rois))
         roi = result.rois[index]
-        print("ROW", index, roi.id.val)
         new_row = {}
         # Create a copy of row, with columns named with_underscores (no spaces)
         for c in col_names:
@@ -103,18 +91,29 @@ def main(conn, args):
     project = conn.getObject("Project", attributes={"name": project_name})
     print("Project", project.id)
     conn.SERVICE_OPTS.setOmeroGroup(project.getDetails().group.id.val)
-    # For each Image in Project, open the local CSV and summarise to one row
-    for dataset in project.listChildren():
-        for image in dataset.listChildren():
-            # ignore _seg images etc.
-            if '_' in image.name:
-                continue
-            if args.name and image.name != args.name:
-                continue
-            if image.getROICount() == 0:
-                print("No ROIs for image", image.name)
-                continue
-            process_image(conn, image)
+    # For each Image in Project, open the local CSV by name...
+    table_names = args.table_names.split(",")
+    errors = []
+    ok = 0
+    for table_name in table_names:
+        print(f"\n\nProcessing table: {table_name}")
+        for dataset in project.listChildren():
+            for image in dataset.listChildren():
+                # ignore _seg images etc.
+                if '_' in image.name:
+                    continue
+                if args.name and image.name != args.name:
+                    continue
+                if image.getROICount() == 0:
+                    print("No ROIs for image", image.name)
+                    continue
+                msg = process_image(conn, image, table_name)
+                if msg is not None:
+                    errors.append(msg)
+                else:
+                    ok += 1
+    print("Successfully attached %s tables to images" % ok)
+    print("\n".join(errors))
 
 
 if __name__ == "__main__":
@@ -122,10 +121,14 @@ if __name__ == "__main__":
     # (and *before* any other ROIs)
     #
     # USAGE: (use --name if you want to ONLY process 1 image)
+    # Comma-separated table names
     # $ cd idr0079-hartmann-lateralline
-    # $ python scripts/csv_to_roi_table.py --name 00E41C184C
+    # $ python scripts/csv_to_roi_table.py --name 00E41C184C _pea3smFISH_RNAcounts_predicted.tsv,_archetype_TFOR_classifications.tsv
     parser = argparse.ArgumentParser()
     parser.add_argument("--name", help="Filter images by Name")
+    parser.add_argument("table_names",
+        help="comma-separated list of table names",
+        default="_other_measurements.tsv")
     args = parser.parse_args()
 
     with omero.cli.cli_login() as c:
